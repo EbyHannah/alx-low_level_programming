d247e25c1c2e4e86b5538f7edecc3c2e5b40f25d
EbyHannah <pinkhannaheby@gmail.com>
2022-09-19 17:00:16 -0700

    did

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,18 +13,27 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+
 	int pass[100];
 
 	int i, sum, n;
 
+
+
 	sum = 0;	
 
+
+
 	srand(time(NULL));
+
+
 
 	for (i = 0; i < 100; i++)
 
 	{
+
 		pass[i] = rand() % 78;
 
 		sum += (pass[i] + '0');
@@ -34,6 +43,7 @@ int main(void)
 		if ((2772 - sum) - '0' < 78)
 
 		{
+
 			n = 2772 - sum - '0';
 
 			sum += n;
@@ -41,7 +51,13 @@ int main(void)
 			putchar(n + '0');
 
 			break;
+
 		}
+
 	}
-	return (0)
+
+
+
+	return (0);
+
 }
